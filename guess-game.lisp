c525a1861d8caba1da04d64013084a0617157ba1
sagasu <sagasu@gmail.com>
2017-06-15 22:20:52 +0200

    adding smaller and bigger function for game

--- guess-game.lisp
+++ guess-game.lisp
@@ -8,3 +8,11 @@
   ;;; and 11 shift to the left is in binary 101 which is 5 in decimal
   ;;; so it is a great technique for binary search
   (ash (+ *small* *big*) -1))
+
+(defun smaller ()
+  (setf *big* (1- (guess-my-number))
+  (guess-my-number)))
+
+(defun bigger ()
+  (setf *small* (1+ (guess-my-number))
+  (guess-my-number)))
