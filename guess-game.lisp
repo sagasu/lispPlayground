812ec2e9f68d384a90722bb02448891779cd2d26
sagasu <sagasu@gmail.com>
2017-06-15 21:43:06 +0200

    playing with binary search on bit manipulation

--- guess-game.lisp
+++ guess-game.lisp
@@ -1,3 +1,10 @@
 (defparameter *small* 1)
 
 (defparameter *big* 100)
+
+(defun guess-my-number ()
+  ;;; this is really interesting binary search technique on bit manipulation
+  ;;; 11 in binary is 1011, if we shift it left it is 10110 and it means in decimal 22 which is x2
+  ;;; and 11 shift to the left is in binary 101 which is 5 in decimal
+  ;;; so it is a great technique for binary search
+  (ash (+ *small* *big*) -1))
